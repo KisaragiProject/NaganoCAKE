1b00e947b9790a5a5236a04e2025a48cd7f81818
Sakinishibe <saki.nishibe@gmail.com>
2020-03-16 16:54:00 +0900

    [add]route(customer側)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ # 顧客用サイトのrouting
+ resources :customers, only: [:edit, :show, :update]
+   get 'customers/withdraw' => 'customers#withdraw', as: 'customer_withdraw_path'
+   patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_path'
+ resources :products, only: [:index, :show]
+ resources :cart_items, only: [:index, :create, :update, :destroy]
+ resources :orders, only: [:new, :index, :create, :show]
+   get'orders/confirm' => 'orders#confirm', as: 'order_confirm_path'
+   get 'orders/thanks' => 'orders#thanks', as: 'order_thanks_path'
+ resources :addresses, only: [:index, :create, :edit, :update, :destroy]
 end
