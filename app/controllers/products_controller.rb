7b8a8817997625cec12aa2aad016d7e8a1f8925c
kohei-iwase <me.tekelily@gmail.com>
2020-03-16 15:59:47 +0000

    products show,products index画面実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
-		@genres = Genre.all
+		@genres = Genre.all #genresバー表示用
 	end
 
 	def index
 		@products = Product.all
+		@genres = Genre.all #genresバー表示用
 	end
 
 
