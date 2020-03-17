aa56f5799cba3866ac63ed73dcf66bc33278d4f5
kohei-iwase <me.tekelily@gmail.com>
2020-03-17 05:18:32 +0000

    header実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_080733) do
+ActiveRecord::Schema.define(version: 2020_03_16_095558) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -76,6 +76,18 @@ ActiveRecord::Schema.define(version: 2020_03_16_080733) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "installs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "order_items", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "product_id", null: false
@@ -85,7 +97,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_080733) do
     t.integer "make_status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["customer_id"], name: "index_order_items_on_customer_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
