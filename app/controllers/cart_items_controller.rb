6e0fabfa55303d0ba492c660068e54b2b81e7453
kohei-iwase <me.tekelily@gmail.com>
2020-03-17 08:44:18 +0000

    cart_item,createアクション定義

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,18 +4,30 @@ class CartItemsController < ApplicationController
   #after_initialize :set_default_values, if: :new_record?
 
 	def create
-		@cart_item = Cart_Item.new
+		@cart_item = Cart_Item.new(cart_item_params)
+		@cart_item.customer_id = current_user.id
+		@cart_item.product_id = 1
+		if @cart_item.save #入力された個数をDBに格納する
+		redirect_to @current_customer_cart_items
+
 	end
 
-	def destory #全部消去メソッドにする
+	def destory #現在ユーザーに紐づいているカートアイテムを全部消去するメソッドにしたい
+		@current_customer_cart_item = current_user.cart_item
+  		@current_customer_cart_item.destoy
+  		redirect_to products_path, notice: "カートを空にしました"
 	end
 
 	def edit
 	end
+	def index
+		@cart_item = Cart_Item.find(params[:cart_item_id])
+		@cart_items = Product.all
+	end
 
 	def show
 #		@customer = Customer.find(params[:id])
-		@cart_item = Cart_Item.find(params[:cart_id])
+		@cart_item = Cart_Item.find(params[:cart_item_id])
 		@cart_items = Product.all
 #		@cart_items = current_customer.cart_items
 	end
@@ -31,10 +43,14 @@ class CartItemsController < ApplicationController
 	end
 
 
-  private
+	 private
 
-  def set_default_values #デフォルトの個数を０に設定
-    self.quanitity   ||= 0
-  end
+		def cart_item_params
+  			params.require(:cart_item).permit(:quanitity)
+  		end
+
+  # def set_default_values #デフォルトの個数を０に設定
+  #   self.quanitity   ||= 0
+  # end
 
 end
