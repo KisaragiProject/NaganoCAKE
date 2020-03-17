0673549b39d9a1eaff8ce1bbb90a1544938230ed
Sakinishibe <saki.nishibe@gmail.com>
2020-03-17 17:23:56 +0900

    [update]application_controller　sign_in, sign_out後のru-to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+	# sign_in後はマイページ遷移（あとでadminと場合分けする）
 	def after_sign_in_path_for(resource)
 		customer_path(resource)
+	end
+
+	# sign_out後は会員用TOPページ遷移（現在は仮でsign_in画面になっています）
+	def after_sign_out_path_for
+		new_customer_session_path
 	end
 end
