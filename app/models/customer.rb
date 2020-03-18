a35575e0e4708988b207dcffe7019f4591d544f0
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-18 10:45:41 +0000

    address関連作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,8 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :cart_items
   has_many :orders
+  has_many :addresses
 
   validates :email, presence: true,
     length: {minimum: 3, maximum: 80}
-  validates :password, presence: true,
-    length: {minimum: 3, maximum: 20}
 end
