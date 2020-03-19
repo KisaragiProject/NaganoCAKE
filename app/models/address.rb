ac75d34c4e99b5f2eb55aede2fa3c4c2222616a5
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-19 05:51:50 +0000

    address入力フォーム

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,9 @@ class Address < ApplicationRecord
 	belongs_to :customer
 
 	validates :post_code, presence: true,
-      length: {minimum: 3, maximum: 10}
+      length: {minimum: 2, maximum: 10}
     validates :addressee, presence: true,
-      length: {minimum: 3, maximum: 35}
+      length: {minimum: 2, maximum: 35}
     validates :address, presence: true,
-      length: {minimum: 3, maximum: 50}
+      length: {minimum: 2, maximum: 50}
 end
