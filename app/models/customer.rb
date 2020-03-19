f01cc9b55703b1c9d12af23b7be0e99703e1e3a0
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-19 05:10:10 +0000

    Revert "Revert "address関連作成""
    This reverts commit a1de95b9ded85162914db4f0e84051082479e2ee.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,9 +6,8 @@ class Customer < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items
   has_many :orders
+  has_many :addresses
 
   validates :email, presence: true,
     length: {minimum: 3, maximum: 80}
-  validates :password, presence: true,
-    length: {minimum: 3, maximum: 20}
 end
