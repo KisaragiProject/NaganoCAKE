a1de95b9ded85162914db4f0e84051082479e2ee
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-19 04:17:44 +0000

    Revert "address関連作成"
    This reverts commit a35575e0e4708988b207dcffe7019f4591d544f0.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,2 @@
 class Address < ApplicationRecord
-	belongs_to :customer
-
-	validates :post_code, presence: true,
-      length: {minimum: 3, maximum: 10}
-    validates :addressee, presence: true,
-      length: {minimum: 3, maximum: 35}
-    validates :address, presence: true,
-      length: {minimum: 3, maximum: 50}
 end
