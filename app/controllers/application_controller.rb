790ecf315b9d16b0be9e84b1417fe78f15019ac8
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-03-19 12:01:09 +0900

    [fix]log_in, log_out動作 正常化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,15 +20,16 @@ class ApplicationController < ActionController::Base
 		customer_path(resource)
 	end
 
+	# sign_out後は会員用TOPページ遷移
+	def after_sign_out_path_for(resource)
+		root_path
+	end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:is_active, :first_name, :first_name_kana, :family_name, :family_name_kana, :post_code, :address, :email, :tel])
     #sign_upの際にnameのデータ操作を許。追加したカラム。
   end
-	# sign_out後は会員用TOPページ遷移（現在は仮でsign_in画面になっています）
-	def after_sign_out_path_for
-		new_customer_session_path
-	end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,7 @@
 class Order < ApplicationRecord
 	has_many :order_item
 
-	validate :addressee, presence: true,
-	  length: {maximum: 2, minimum: 35}
-	validate :post_code, presence: true,
-	  length: {maximum: 3, minimum: 10}
-	validate :send_to_address, presence: true,
-	  length: {maximum: 3, minimum: 50}
+	validate :addressee, length: {maximum: 2, minimum: 35}
+	validate :post_code, length: {maximum: 3, minimum: 10}
+	validate :send_to_address, length: {maximum: 3, minimum: 50}
 end
