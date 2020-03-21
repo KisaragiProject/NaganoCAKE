b4d1e28d78f3d2043d207c614fd8138176b48830
kohei-iwase <me.tekelily@gmail.com>
2020-03-21 06:51:01 +0000

    ジャンル追加機能実装

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,26 +1,7 @@
 class GenresController < ApplicationController
-	def create
-		@genre = Genre.new(genre_params)
-		if @genre.save #入力されたデータをdbに保存する。
-  		redirect_to :index, notice: "successfully created genre!"#保存された場合の移動先を指定。
-	end
 	def index
-		Genre.new
 		@genres = Genre.all
 	end
-
-	def edit
-		@genre = Genre.find(params[:id])
-	end
-
-	def update
-  	@genre = Genre.find(params[:id])
-  	if @genre.update(genre_params)
-  		redirect_to @genre, notice: "successfully edit"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-        render "edit", notice: "edit error"
-  	end
-  end
 
 	def genre_params
   	params.require(:genre).permit(:name,:id)
