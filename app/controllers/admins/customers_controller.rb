2a93e5183f2545e0a7078a97492e4f408043a86d
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-03-21 10:33:55 +0000

    adimin_customer関連view作成

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,31 @@
 class Admins::CustomersController < ApplicationController
+	def index
+		@customers = Customer.all
+	end
+
+	def show
+		@customer = Customer.find(params[:id])
+	end
+
+	def edit
+		@customer = Customer.find(params[:id])
+
+	end
+
+	def update
+		@customer = Customer.find(params[:id])
+		if
+		  @customer.update(customer_params)
+		  flash[:success] = "更新に成功しました"
+		  redirect_to admins_customer_path
+		else
+		  flash[:warning] = "入力内容を確認してください"
+		  render :edit
+		end
+	end
+
+	private
+	  def customer_params
+	  	params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :tel, :email, :is_active)
+	  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-　　Admin.create!( id: 1, email:'q@p',
+	Admin.create!( id: 1, email:'q@p',
 				  password: "333333",
 				  password_confirmation: "333333"
 	             )
