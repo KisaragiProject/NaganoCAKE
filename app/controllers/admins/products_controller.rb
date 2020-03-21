a347114f58b5a379f62a0c3a213041fe60ea2266
kohei-iwase <me.tekelily@gmail.com>
2020-03-21 08:29:25 +0000

    [create] 新規商品登録画面

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,7 +1,36 @@
 class Admins::ProductsController < ApplicationController
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+		redirect_to admins_products_path
+		else
+		render :new
+		end
+	end
 
 	def index
 		@products = Product.all
 	end
 
+	def show
+    	@product = Product.find(params[:id])
+    end
+
+	def edit
+    	@product = Product.find(params[:id])
+    end
+
+
+	private
+	def product_params
+		params.require(:product).permit(:name, :genre_id, :image, :introduction, :status, :price)
+	end
+
+	def genre_params
+		params.require(:genre).permit(:name)
+	end
 end
