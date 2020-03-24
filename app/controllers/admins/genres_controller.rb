8a95dd5384616bf4f7490704a28de4c41612d4ea
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-03-24 17:15:33 +0900

    [add]admins/genre flash_messages

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -3,9 +3,14 @@ class Admins::GenresController < ApplicationController
 	def create
 		@genre = Genre.new(genre_params)
 		if @genre.save #入力されたデータをdbに保存する。
-  		redirect_to admins_genres_path, notice: "successfully created genre!"#保存された場合の移動先を指定。
-	end
-end
+  		   redirect_to admins_genres_path, success: "新しいジャンルが作成されました！"#保存された場合の移動先を指定。
+	    else
+	      @genres = Genre.all
+	      flash[:danger] = 'ジャンルの作成に失敗しました。名前の入力欄は空白になっていませんか？'
+	      render :index
+	    end
+    end
+
 	def index
 		@genre = Genre.new
 		@genres = Genre.all
@@ -17,15 +22,16 @@ end
 
 	def update
   	@genre = Genre.find(params[:id])
-  	if @genre.update(genre_params)
-  		redirect_to admins_genres_path, notice: "successfully edit"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-        render "edit", notice: "edit error"
-  	end
-  end
+  	  if @genre.update(genre_params)
+  		redirect_to admins_genres_path, success: "ジャンルの情報が更新されました！"
+  	  else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+        flash[:danger] = "ジャンルの情報は更新されませんでした。名前の入力欄は空白になっていませんか？"
+  	    render :edit
+  	  end
+    end
 
 	def genre_params
   	params.require(:genre).permit(:name, :validity)
-  end
+    end
 end
 
