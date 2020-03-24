427480b66ba7b0f015518d3156d03fba019a3e6d
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-03-24 17:38:35 +0900

    [update]admins/products flash_message, validation

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -6,9 +6,10 @@ class Admins::ProductsController < ApplicationController
 
 	def create
 		@product = Product.new(product_params)
-		if @product.save
-		redirect_to admins_products_path
+		if @product.save(product_params)
+		redirect_to admins_products_path, success: '新しい商品データが作成されました！'
 		else
+		flash[:danger] = '商品データを作成できませんでした。商品写真の選び忘れや空欄はありませんか？'
 		render :new
 		end
 	end
@@ -28,9 +29,10 @@ class Admins::ProductsController < ApplicationController
     def update
   	@product = Product.find(params[:id])
   	if @product.update(product_params)
-  		redirect_to admins_products_path, notice: "successfully edit"
+  		redirect_to admins_products_path, success: '商品データを更新しました！'
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-        render "edit", notice: "edit error"
+        flash[:danger] = "商品データを更新できませんでした。空欄になっている箇所はありませんか？"
+  		render :edit
   	end
   end
 
