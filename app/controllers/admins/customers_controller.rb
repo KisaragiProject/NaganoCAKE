4c27789da96f3cb02d9fa0328c54402cc7b468a4
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-03-24 16:54:57 +0900

    [add]admins/customer validation, flash_messages

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -13,23 +13,21 @@ class Admins::CustomersController < ApplicationController
 
 	def edit
 		@customer = Customer.find(params[:id])
-
 	end
 
 	def update
 		@customer = Customer.find(params[:id])
 		if
 		  @customer.update(customer_params)
-		  flash[:success] = "更新に成功しました"
-		  redirect_to admins_customer_path
+		  redirect_to admins_customer_path(@customer), success: '更新に成功しました！'
 		else
-		  flash[:warning] = "入力内容を確認してください"
+		  flash[:danger] = "入力内容を確認してください"
 		  render :edit
 		end
 	end
 
 	private
 	  def customer_params
-	  	params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :tel, :email, :is_active)
+	  	params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :tel, :email, :address, :is_active)
 	  end
 end
