63829b07034ec9a45fc2144543a8db3c572125aa
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-03-24 13:06:38 +0900

    .

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,22 @@
 class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
-		@genres = Genre.all #genresバー表示用
+		@genres = Genre.where(validity: true) #genresバー表示用
 		@cart = @product.cart_items.build #モデルなのでbuildを使用
 	end
 
 	def index
-		@genres = Genre.all #genresバー表示用
-
+		@genres = Genre.where(validity: true) #genresバー表示用
 		#リンクをクリックするとジャンルidを取得
 		@genre_id = params[:genre_id]
 		# @products = Product.where(params[genre_id])
-		#ジャンリウidがある場合、ジャンルidのプロダクトだけを表示
+		#ジャンルidがある場合、ジャンルidのプロダクトだけを表示
 		if params[:genre_id].present?
 		    @products = @products.get_by_genre_id params[:genre_id]
 	      # @selected_genre = Genre.find(params[:genre_id])
     	  # @producs= Product.from_genre(params[:genre_id]).page(params[:page])
     	else
-       	 @products= Product.all.page(params[:page])
-
+       	 @products= Product.where(status: true)
 		end
 
 	end
